1fd2828a8947f26f92f661de4ceb3d7c2ac9f3f0
Ubuntu <ubuntu@ip-172-31-83-215.ec2.internal>
2025-07-16 19:37:33 +0000

    nginx

--- apps/excalidraw-frontend/config.ts
+++ apps/excalidraw-frontend/config.ts
@@ -1,2 +1,2 @@
 export const HTTP_ENDPOINT = "https://canvas.nitinxdev.fun/api";
-export const WS_ENDPOINT = "wss://canvas.nitinxdev.fun/ws";
+export const WS_ENDPOINT = "wss://ws-canvas.nitinxdev.fun";
